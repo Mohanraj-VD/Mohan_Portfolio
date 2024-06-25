e247d18ffd2c1eb8d7c545ea444c3acf8150897e
Mohanraj <mohanrajvdmm@gmail.com>
2024-06-25 16:16:52 +0530

    Title Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Profile from './Component/Profile.jsx';
+import React, { useEffect } from 'react';
 function App() {
+  useEffect(() => {
+    document.title = 'Mohanraj\'s Portfolio';
+    }, []);
   return (
     <>
-    
     <div className="App">
       <Profile name="Mohanraj V"/>
     </div>
