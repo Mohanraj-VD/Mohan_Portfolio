e1fd4f84da5da744673a095ddfa8ea9e8dcbecc1
Mohanraj <mohanrajvdmm@gmail.com>
2024-07-07 17:49:18 +0530

    Navbar styles update

--- src/Component/Navb.jsx
+++ src/Component/Navb.jsx
@@ -14,13 +14,13 @@ function Navb() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav btn-close">
           <Nav className="me-auto" >
-            <Nav.Link href="#ovw" id="nv1">Home</Nav.Link>
+            <Nav.Link href="#ovw" id="nv2">Home</Nav.Link>
             <Nav.Link href="#abt" id="nv2">About</Nav.Link>
             <Nav.Link href="#edu" id="nv2">Education</Nav.Link>
             <Nav.Link href="#exp" id="nv2">Experience</Nav.Link>
             <Nav.Link href="#skill" id="nv2">Skills</Nav.Link>
             <Nav.Link href="#prj" id="nv2">Projects</Nav.Link>
-            <Nav.Link href="#cert" id="nv2">Courses/Certifications</Nav.Link>
+            <Nav.Link href="#cert" id="nv2">Certifications</Nav.Link>
             <NavDropdown id="nv2" title="Profile">
               <NavDropdown.Item href="https://linkedin.com/in/mohanraj-v-java-developer" target='blank' id="dropdown">Linked In</NavDropdown.Item>
               <NavDropdown.Item href="https://github.com/Mohanraj-VD" target='blank' id="dropdown">
